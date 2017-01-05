9d9917d075398a4cb6c124fb21df1db7ed0839d8
BrianLusina <lusinabrian@gmail.com>
2017-01-05 23:02:31 +0300

    add portfolio grid styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
-import './styles/font-awesome.min.css'
+import './styles/font-awesome.min.css';
+import './styles/normalize.css';
 
 
 ReactDOM.render(
