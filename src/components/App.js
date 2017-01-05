9bf35e82b36e5e9ad58c3114b65b777c96c9c9fc
BrianLusina <lusinabrian@gmail.com>
2017-01-05 23:05:15 +0300

    move fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import '../styles/App.css';
 import Header from './Header';
 import About from './About';
 import Portfolio from './Portfolio';
