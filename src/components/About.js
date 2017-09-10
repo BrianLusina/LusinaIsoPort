ae5bb13f6907d535ddeb43daadf6fe1dafb461a7
Brian Lusina <lusinabrian@gmail.com>
2017-09-10 10:10:16 +0300

    convert about component to stateless

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,18 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class About extends Component {
-    render() {
-        return (
-            <a className="pater" href="#">
-                <h2 className="pater__title" aria-label="About"></h2>
-                <p className="pater__desc">
-                    I am Brian Lusina, an software developer.
-                    Creating sites and apps that are both simple and fun to use is my passion.
-                </p>
-            </a>
-        )
-    }
-}
-        
+/**
+ * About stateless component
+ * */
+const About = () => {
+    return (
+        <a className="pater" href="#">
+            <h2 className="pater__title" aria-label="About"/>
+            <p className="pater__desc">
+                I am Brian Lusina, an software developer.
+                Creating sites and apps that are both simple and fun to use is my passion.
+            </p>
+        </a>
+    );
+};
+
+export default About
