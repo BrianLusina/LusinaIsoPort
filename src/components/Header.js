77b41142ed7386c4b28705767b6c690407b7554b
Brian Lusina <lusinabrian@gmail.com>
2017-09-10 10:06:10 +0300

    Convert header to stateless component
    This held no state and simply rendered a page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Header extends Component {
+/**
+ * Header stateless component
+ */
 // <button className="button-menu">
 //  <svg width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 67.75 43.63"><rect width="67.75" height="8.38"/><rect x="4.15" y="17.63" width="59.45" height="8.38" /><rect y="35.25" width="67.75" height="8.38"/></svg>
 // </button>
+const Header = () => {
+    return (
+        <header className="codrops-header">
+            <h1>The Lusina</h1>
 
-    render() {
-        return (
-            <header className="codrops-header">
-                <h1>The Lusina</h1>
+            <p>Software developer.</p>
 
-                <p>Software developer.</p>
+            <div className="codrops-links">
 
-                <div className="codrops-links">
+                <a className="codrops-icon codrops-icon--resume"
+                   href="http://lusinaresume.netlify.com/" target="_blank" title="Resume">
+                    <span>Resume</span>
+                </a>
 
-                    <a className="codrops-icon codrops-icon--resume"
-                       href="http://lusinaresume.netlify.com/" target="_blank" title="Resume">
-                        <span>Resume</span>
-                    </a>
+                <a className="codrops-icon codrops-icon--contact" href="#" title="Contact">
+                    <span>Contact</span>
+                </a>
 
-                    <a className="codrops-icon codrops-icon--contact" href="#" title="Contact">
-                        <span>Contact</span>
-                    </a>
+            </div>
+        </header>
+    );
+};
 
-                </div>
-            </header>
-        )
-    }
-}
+
+export default Header;
